84cb302cd060cf4467d165f20cb344f0d02e1861
Joshua Ow <joshuaow@gmail.com>
2023-12-15 11:05:32 -0600

    added professional solution with my own

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,19 +1,49 @@
+//public class RewardValue {
+//    private final double cashValue;
+//    private final double milesValue;
+//
+//    public RewardValue(double cashValue) {
+//        this.cashValue = cashValue;
+//        this.milesValue = cashValue * 0.0035;
+//    }
+//
+//    public double getCashValue() {
+//        return cashValue;
+//    }
+//
+//    public double getMilesValue() {
+//        return milesValue;
+//    }
+//
+//
+//}
+
+
 public class RewardValue {
     private final double cashValue;
-    private final double milesValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = cashValue * 0.0035;
+    }
+
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
-        return milesValue;
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
     }
-
-
 }
